23035e33b15efe4f986160a48c52f94bd486ebf5
Wole Ekanola <woleekanola@gmail.com>
2026-07-18 08:42:15 +0100

    squash all migrations into single init migration for prod

--- prisma/migrations/20260709140637_add_otp_attempts/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "OTP" ADD COLUMN     "attempts" INTEGER NOT NULL DEFAULT 0;
--- prisma/migrations/20260710010000_add_bed_status/migration.sql
+++
@@ -1,3 +0,0 @@
--- CreateEnum
-CREATE TYPE "BedStatus" AS ENUM ('AVAILABLE', 'OCCUPIED', 'MAINTENANCE');
-
